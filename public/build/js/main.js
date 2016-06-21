f26c3034d0da8eac610976a6d830cba2a1d3999a
Yin Sheng Zhou <yinsheng@sg.ibm.com>
2016-06-21 14:45:12 +0800

    complete framework

--- public/build/js/main.js
+++ public/build/js/main.js
@@ -708,27 +708,37 @@ var $ = require('jquery');
 
 //Below function Executes on click of login button
 function validate(){
-    console.log("validate");
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
+    
+    $.ajax({
+        type : "POST",
+        url : "checkLogin",
+        dataType: 'json',
+        data: { userId : username, pass: password},
+        success : function(data) {
+            var len = data.length;        
+            console.log(data);
+        }
+    });
 
-	if ( username == "Formget" && password == "formget#123"){
-		alert ("Login successfully");
-		window.location = "index.html"; //redirecting to other page
-		return false;
-	}
-	else{
-		attempt --;//Decrementing by one
-		alert("You have left "+attempt+" attempt;");
-		
-		//Disabling fields after 3 attempts
-		if( attempt == 0){
-			document.getElementById("username").disabled = true;
-			document.getElementById("password").disabled = true;
-			document.getElementById("submit").disabled = true;
-			return false;
-		}
-	}
+	//if ( username == "mpa" && password == "mpa"){
+	//	alert ("Login successfully");
+	//	window.location = "map.html"; //redirecting to other page
+	//	return false;
+	//}
+	//else{
+	//	attempt --;//Decrementing by one
+	//	alert("You have left "+attempt+" attempt;");
+	//	
+	//	//Disabling fields after 3 attempts
+	//	if( attempt == 0){
+	//		document.getElementById("username").disabled = true;
+	//		document.getElementById("password").disabled = true;
+	//		document.getElementById("submit").disabled = true;
+	//		return false;
+	//	}
+	//}
 }
 
 $('#submit').click(validate);
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,27 +3,58 @@ var $ = require('jquery');
 
 //Below function Executes on click of login button
 function validate(){
-    console.log("validate");
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
+    
+    $.ajax({
+        type : "POST",
+        url : "http://localhost:8080/SAFER_REST/checkLogin",
+        dataType: 'json',
+        data: { userId : username, pass: password},
+        success : function(data) {
+            var len = data.length;        
+            console.log(data);
+            if (data.hasOwnProperty('error'))
+                alert(data.error);
+            if( data.isValidLogin == true ) {
+		        alert ("Login successfully");
+                localStorage.setItem('userId', data.userId);
+                localStorage.setItem('userModules', data.userModules);
+		        window.location = "map.html"; //redirecting to other page
+		        return false;
+            } else {
+	        	attempt --;//Decrementing by one
+	        	alert("Invalid username and password! You have left "+attempt+" attempt;");
+	        	
+	        	//Disabling fields after 3 attempts
+	        	if( attempt == 0){
+	        		document.getElementById("username").disabled = true;
+	        		document.getElementById("password").disabled = true;
+	        		document.getElementById("submit").disabled = true;
+	        		return false;
+	        	}
+            }
 
-	if ( username == "mpa" && password == "mpa"){
-		alert ("Login successfully");
-		window.location = "index.html"; //redirecting to other page
-		return false;
-	}
-	else{
-		attempt --;//Decrementing by one
-		alert("You have left "+attempt+" attempt;");
-		
-		//Disabling fields after 3 attempts
-		if( attempt == 0){
-			document.getElementById("username").disabled = true;
-			document.getElementById("password").disabled = true;
-			document.getElementById("submit").disabled = true;
-			return false;
-		}
-	}
+        }
+    });
+
+	//if ( username == "mpa" && password == "mpa"){
+	//	alert ("Login successfully");
+	//	window.location = "map.html"; //redirecting to other page
+	//	return false;
+	//}
+	//else{
+	//	attempt --;//Decrementing by one
+	//	alert("You have left "+attempt+" attempt;");
+	//	
+	//	//Disabling fields after 3 attempts
+	//	if( attempt == 0){
+	//		document.getElementById("username").disabled = true;
+	//		document.getElementById("password").disabled = true;
+	//		document.getElementById("submit").disabled = true;
+	//		return false;
+	//	}
+	//}
 }
 
 $('#submit').click(validate);
