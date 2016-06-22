4b4c3a3b70b1b8f0d624deb2fe70ec92055f90a2
Yin Sheng Zhou <yinsheng@sg.ibm.com>
2016-06-22 16:54:27 +0800

    edit readme and gitignore

--- target/SAFER/images/rotate.py
+++
@@ -1,6 +0,0 @@
-import subprocess,sys
-
-for i in range(1,360):
-    convert = "convert ship0.png -distort ScaleRotateTranslate {} ship{}.png".format(i, i) 
-    print convert
-    subprocess.call(convert, shell=True)
