e58637a7c1dd2a0e03d65823da1fd93f325c8f9b
Yin Sheng Zhou <yinsheng@sg.ibm.com>
2016-07-05 16:46:24 +0800

    1. add leaflet.css into highUtil stylesheet folder 2. merge leaflet.scss into highUtil.min.css to minimize the resource call

--- public/build/js/main.js
+++ public/build/js/main.js
@@ -694,12 +694,12 @@ var simulationEngine = require('./simulationEngine.js');
 ui.setUpBindingButtons();
 
 map.drawSgGisMap(function(){
-    $.get("/api/data/", function(data, status){
+//    $.get("/api/data/", function(data, status){
 //    $.get("http://localhost:8080/SAFER/sample", function(data, status){
-        simulationEngine.animate(data, 1435037400);
-    });
+//        simulationEngine.animate(data, 1435037400);
+//    });
     console.log("Drawing finishes!");
-	map.initCanvasOverlay();
+//  map.initCanvasOverlay();
 });
 
 $('.title').text('Sense Making');
